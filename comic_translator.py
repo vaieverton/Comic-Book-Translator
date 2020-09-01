536f1107ab6130898855c093e2eacbf4e09ef23b
Everton S. Junior <52728867+vaieverton@users.noreply.github.com>
2020-08-31 23:09:10 -0400

    Update comic_translator.py

--- comic_translator.py
+++ comic_translator.py
@@ -5,14 +5,12 @@ from googletrans import Translator
 translator = Translator()
 import unidecode
 
-# Here you define your native language
+# Aqui você define sua lingua
 # "ko" = korean | "en" = english | "ar" = arabic |
-# In my case "pt" for portuguese
+#  No nosso caso, "pt" - Português
 targeted_lang= "pt"
 
-# main function to perform the translation
-
-
+# função principal para traduzir a HQ
 def put_text_page(data, img):
     font = cv.FONT_HERSHEY_SIMPLEX
     for x,b in enumerate(data.splitlines()):
@@ -20,39 +18,45 @@ def put_text_page(data, img):
            b = b.split()
            if len(b)==12:
                 x,y,w,h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
+                
                 cv.rectangle(img,(x , y), (w+x, h+y), (255,255,255), -1)
-                word = b[11]
-                word_translated = translator.translate(text=word, dest=targeted_lang) # tranlation
-                word_translated = unidecode.unidecode(word_translated.text) # remove weird shit
-                cv.putText(img, word_translated, (x+10, y+40), font, 1, (0, 0, 0), 1) # put text on the screen
 
-# function that read the image with pytesseract
+                word = b[11]
+                
+                word_translated = translator.translate(text=word, dest=targeted_lang) # Tradução
+                
+                word_translated = unidecode.unidecode(word_translated.text) # Formata o texto
+                
+                cv.putText(img, word_translated, (x+10, y+40), font, 1, (0, 0, 0), 1) # Insere o texto traduzido na tela
+
+# função que extrai o texto da tela com tesseract ocr
 def read_text(img):
     data = pytesseract.image_to_data(img)
     print(data)
     return data
 
 def main():
-    # here you define the number of the comic page with want to start translating
-    # and the final page number
-    # in my case, page 2 | page 10
-    number_initial_page = 2
-    number_final_page = 3
+    # Aqui você define quantas paginas tem a HQ
+    # Digite o numero da ultima pagina
+    
+    number_initial_page = 1
+    number_final_page = 8
 
-    # loop through every page and translate it
+    # loop por cada página e traduza-a..
     while number_initial_page < number_final_page:
         num = str(number_initial_page)
-        img = cv.imread('Dexter Down Under 002-00'+num+'.jpg')
+        img = cv.imread('nome_hq_page_'+num+'.jpg')
         img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
         data = read_text(img)
         put_text_page(data, img)
 
-        cv.imwrite('Page Translated'+num+'.png', img)
-        print("PAGE "+num+" COMPLETED")
+
+        cv.imwrite('Página Traduzida'+num+'.png', img)
+        print("Página "+num+" Traduzida.")
 
         number_initial_page += 1
 
-    print('COMIC-BOOK TRANSLATED SUCCESSFULLY!')
+    print('HQ traduzida com sucesso!')
 
 main()
